4f2522b87cc796df423a01d26efdb56bb5ec4c6c
Minseo Lee <itoupluk427@gmail.com>
2024-11-12 00:52:41 +0900

    Update pm2.config.cjs

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   name: "kbo-labeler", // Name of your application
-  script: "src/index.ts", // Entry point of your application
+  script: "src/main.ts", // Entry point of your application
   interpreter: "bun", // Bun interpreter
   env: {
     PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
