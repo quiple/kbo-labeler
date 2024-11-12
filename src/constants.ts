539ef47af7609554c9ad505a57a97d19a22b00d5
Minseo Lee <itoupluk427@gmail.com>
2024-11-12 16:04:14 +0900

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,14 @@ export const LABEL_LIMIT = 1;
 export const LABELS: Label[] = [
   {
     rkey: '3laq35afvtc22',
+    identifier: 'hanwha-eagles-2',
+    locales: [
+      { lang: 'en', name: 'Hanwha Eagles2', description: 'This user is a fan of Hanwha Eagles.'},
+      { lang: 'ko', name: '한화 이글스2', description: '이 사용자는 한화 이글스의 팬입니다.'},
+    ]
+  },
+  {
+    rkey: '3laom2ohwal2e',
     identifier: 'hanwha-eagles',
     locales: [
       { lang: 'en', name: 'Hanwha Eagles', description: 'This user is a fan of Hanwha Eagles.'},
